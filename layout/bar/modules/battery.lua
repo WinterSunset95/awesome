bcb27287da9aa453a94703903652e6887099af5a
WinterSunset95 <wintersunset95@gmail.com>
2025-09-11 00:33:10 +0530

    feat(battery): added listeners for low battery and charging status

--- layout/bar/modules/battery.lua
+++ layout/bar/modules/battery.lua
@@ -79,11 +79,46 @@ local function battery_color(battery)
 	end
 end
 
-awful.widget.watch('bash -c "cat ' .. battery_path .. '/capacity"', 10, function(_, stdout)
-	batttery_widget:set_value(tonumber(stdout) or 0)
-	batttery_widget:set_colors({ battery_color(tonumber(stdout) or 0) })
-	local mytextbox = batttery_widget:get_children_by_id("text_role")[1]
-	mytextbox:set_text(stdout or 0)
-end, batttery_widget)
+gears.timer({
+  timeout = 10,
+  autostart = true,
+  call_now = true,
+  callback = function()
+    local bat_status2 = io.open(battery_path .. "/status")
+    local charge2 = io.open(battery_path .. "/capacity")
+    if bat_status2 == nil or charge2 == nil then
+      naughty.notify({
+        title = "Error",
+        text = "Could not get battery status or charge",
+      })
+      return
+    end
+    local battery_level = tonumber(charge2:read("*all")) or 0
+    local mytextbox = batttery_widget:get_children_by_id("text_role")[1]
+    mytextbox.text = tostring(battery_level)
+    local charge_status = bat_status2:read("*all")
+    batttery_widget.value = battery_level
+    if charge_status:match("Charging") then
+      batttery_widget:set_colors({ "#00ff00" })
+    elseif charge_status:match("Discharging") then
+      batttery_widget:set_colors({ battery_color(battery_level) })
+    end
+    if battery_level < 50 and charge_status:match("Discharging") then
+      naughty.notify({
+        title = "Battery low",
+        text = "Battery level is at " .. battery_level .. "%",
+        timeout = 5,
+      })
+    elseif battery_level < 20 and charge_status:match("Discharging") then
+      naughty.notify({
+        title = "Battery critical",
+        text = "Battery level is at " .. battery_level .. "%",
+        preset = naughty.config.presets.critical,
+      })
+    end
+    bat_status2:close()
+    charge2:close()
+  end,
+})
 
 return batttery_widget
